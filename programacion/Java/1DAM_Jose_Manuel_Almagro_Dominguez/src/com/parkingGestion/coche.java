2f57b08cd30e8dec661bebba94bd30ca0e644de3
LysergMoonlighting <68394160+LysergMoonlighting@users.noreply.github.com>
2021-04-08 12:36:29 +0200

    push cambios en programa
    se han subidos los cambios realizados con jesus

--- programacion/Java/1DAM_Jose_Manuel_Almagro_Dominguez/src/com/parkingGestion/coche.java
+++ programacion/Java/1DAM_Jose_Manuel_Almagro_Dominguez/src/com/parkingGestion/coche.java
@@ -2,6 +2,11 @@ package com.parkingGestion;
 
 public class coche extends vehiculo{
 
+	public coche(String matricula, String marca, String color) {
+		super(matricula, marca, color);
+		// TODO Auto-generated constructor stub
+	}
+
 	
 	
 	
