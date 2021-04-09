2b4a9d5286545aa8e9a525bec1de6dd7c7249970
LysergMoonlighting <68394160+LysergMoonlighting@users.noreply.github.com>
2021-04-09 11:58:19 +0200

    cambios mayores en la matrix

--- programacion/Java/1DAM_Jose_Manuel_Almagro_Dominguez/src/com/parkingGestion/furgoneta.java
+++ programacion/Java/1DAM_Jose_Manuel_Almagro_Dominguez/src/com/parkingGestion/furgoneta.java
@@ -2,6 +2,11 @@ package com.parkingGestion;
 
 public class furgoneta extends vehiculo{
 
+	public furgoneta(String matricula, String marca, String color) {
+		super(matricula, marca, color);
+		// TODO Auto-generated constructor stub
+	}
+
 	
 }
 
